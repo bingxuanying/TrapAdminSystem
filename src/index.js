5a84d2e3fde7ef1c6840217018e2e50f610b3088
bingxuan.ying <yingbingxuan@gmail.com>
2019-11-19 08:46:42 +0000

    added err pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 let express = require("express")
 let app = express()
-let personRoute = require('./routes/person')
+let personRoute = require("./routes/person")
+let dateFormat = require("dateformat")
+let path = require("path")
 
+app.use(function(req, res, next) {
+    var myDate = new Date()
+    myDate.setHours(myDate.getHours() - 8)
+    var myDateFormat = dateFormat(myDate, "mm-dd-yyyy HH:MM:ss")
+    console.log(`${myDateFormat} => ${req.originalUrl}`)
+    next()
+})
 app.use(personRoute)
-app.use(express.static('public'))
+app.use(express.static("public"))
+
+app.use(function(req, res, next) {
+    res.status(404).send("You are LOST")
+})
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 
 let PORT = process.env.PORT || 3000
 app.listen(PORT, function() {
--- src/routes/person.js
+++ src/routes/person.js
@@ -2,7 +2,19 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/person', function(req, res) {
-    res.send('You request a person')
+    if(req.query.name) {
+        res.send(`You request a person ${req.query.name}`)
+    }
+    else{
+        res.send('You request a person')
+    }
 })
 
+router.get('/person/:name', function(req, res) {
+    res.send(`You request a person ${req.params.name}`)
+})
+
+router.get('/error', function(req, res) {
+    throw new Error("This is a forced error")
+})
 module.exports = router
