9108592a71bc1461f2c30ac81410b65c62d2e7d0
Bingxuan Ying <yingbingxuan@gmail.com>
2019-12-03 02:46:25 -0800

    Test on connecting DB

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,25 +2,22 @@ let express = require("express");
 let router = express.Router();
 
 // get index page
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.render('index', {title:"My Application"})
 })
 
 // Post login data
-router.get("/login", function(req, res, next){
+router.post("/login", (req, res, next) => {
   res.json(req.body)
 })
 
 // Post register data
-/*
-TODO: transfer to register page
-*/
-router.get("/register", function(req, res, next) {
-  res.send("WORKING")
+router.post("/register", (req, res, next) => {
+  res.json(req.body)
 })
 
 // test
-router.get("/trap", function (req, res) {
+router.get("/trap", (req, res) => {
   if (req.query.id) {
     res.send(`You have requested trap ID #${req.query.id}`)
   } 
@@ -29,7 +26,7 @@ router.get("/trap", function (req, res) {
   }
 });
 
-router.get("/trap/:id", function (req, res) {
+router.get("/trap/:id", (req, res) => {
   res.send(`You have requested trap ID #${req.params.id}`)
 })
 
