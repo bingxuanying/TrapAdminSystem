ec5dcbfe84037ada33d7bc7e9cbb9ef83f064bc3
Bingxuan Ying <yingbingxuan@gmail.com>
2019-12-12 22:53:16 -0800

    Add item MONITORING under menu nav

--- client/src/component/bars/SideBar.js
+++ client/src/component/bars/SideBar.js
@@ -4,7 +4,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faCircle,
-  faAngleLeft
+  faAngleLeft,
+  faCamera,
+  faChartPie
 } from "@fortawesome/free-solid-svg-icons";
 import "./Side.css";
 
@@ -65,6 +67,32 @@ class SideBar extends Component {
               {this.props.toggleOn && (
                 <div>
                   <span className="mainmenu-middle">Dashboard</span>
+                  <span className="mainmenu-right">
+                    <FontAwesomeIcon
+                      icon={faAngleLeft}
+                      size="xs"
+                    ></FontAwesomeIcon>
+                  </span>
+                </div>
+              )}
+            </a>
+          </li>
+          {/* #2 Monitoring */}
+          <li
+            className={
+              this.props.toggleOn === true
+                ? "mainmenu-home-lg"
+                : "mainmenu-home-sm"
+            }
+          >
+            <a href="#">
+              <FontAwesomeIcon
+                className="mainmenu-fa"
+                icon={faCamera}
+              ></FontAwesomeIcon>
+              {this.props.toggleOn && (
+                <div>
+                  <span className="mainmenu-middle">Monitoring</span>
                   <span className="mainmenu-right">
                     <FontAwesomeIcon
                       icon={faAngleLeft}
