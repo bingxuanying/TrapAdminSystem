b5ad39177d89c221b2ede22dfec66d838d02194f
Bingxuan Ying <yingbingxuan@gmail.com>
2019-12-18 22:27:41 -0800

    Add webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /.js?$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-react", "@babel/preset-env"]
+        }
+      }
+    ]
+  }
+};
