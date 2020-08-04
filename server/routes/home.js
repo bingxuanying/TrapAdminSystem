b125a18d7cb5c36aedc2294c95c13c56a8a89a68
Bingxuan Ying <yingbingxuan@gmail.com>
2020-08-03 21:04:56 -0700

    Delete unused module

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const mongoose = require("mongoose");
 const User = require("../models/users-model");
 const bcrypt = require("bcrypt");
-const passport = require("passport");
 const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 require("dotenv/config");
